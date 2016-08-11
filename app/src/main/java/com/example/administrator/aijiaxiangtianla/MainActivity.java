b4198e03211571ec6ed4a0fc082d50bdba2b5d59
yangzhuoxinandroid <524283264@qq.com>
2016-08-11 20:33:06 +0800

    ss

--- app/src/main/java/com/example/administrator/aijiaxiangtianla/MainActivity.java
+++ app/src/main/java/com/example/administrator/aijiaxiangtianla/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.aijiaxiangtianla;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,14 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+  public LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
     public void click(View view){
         switch (view.getId()){
@@ -30,5 +31,6 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+
 
 }
