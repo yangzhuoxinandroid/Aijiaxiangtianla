275b4b7e80015031c1e9d222023a18c310724c65
1317972280 <1317972280@qq.com>
2016-08-11 16:46:26 +0800

    vsdvwfwegdb

---
+++ main/src/androidTest/java/com/example/lovehome/HomeMainActivity.java
@@ -0,0 +1,12 @@
+package com.example.lovehome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+public class HomeMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_main);
+    }
+}
